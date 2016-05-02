d8b5cc0ec991931e15b2c603f090330113964296
wune268 <zzwu@zzwudemac.local>
2016-05-02 20:05:16 +0800

    Initial Commit

---
+++ zw_transmission/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zw_transmission
+//
+//  Created by zzwu on 16/5/2.
+//  Copyright © 2016年 zzwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zw_transmission/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zw_transmission
+//
+//  Created by zzwu on 16/5/2.
+//  Copyright © 2016年 zzwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
